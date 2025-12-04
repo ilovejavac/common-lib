48c2bdf580edf53d91ba697a440b069210266fb1
eicen <lc024437@gmail.com>
2025-12-04 08:39:35 +0800

    feat: ddd

---
+++ common-core/src/main/java/com/dev/lib/domain/AggregateRoot.java
@@ -0,0 +1,4 @@
+package com.dev.lib.domain;
+
+public interface AggregateRoot {
+}
--- common-core/src/main/java/com/dev/lib/domain/BaseID.java
+++ common-core/src/main/java/com/dev/lib/domain/BaseID.java
@@ -1,6 +1,7 @@
 package com.dev.lib.domain;
 
 import com.dev.lib.entity.id.IDWorker;
+import com.dev.lib.entity.id.IntEncoder;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -10,8 +11,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public class BaseID {
     private final Long id;
+    private final String bizId;
 
     protected BaseID() {
         id = IDWorker.nextID();
+        bizId = IntEncoder.encode36(id);
     }
 }
