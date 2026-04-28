0f3057fb0d3f495c00b3d0340c17f0ffba4aba5b
程 <1106816442@qq.com>
2026-04-28 18:56:24 +0800

    chore: bump release candidate versions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env sh
 set -eu
 
-exec java $JVM_OPTS \
-  -Dserver.port=8080 \
-  -jar /app/app.jar
+exec java ${JVM_OPTS:-} \
+  -Dserver.port="${SERVER_PORT:-8080}" \
+  -Duser.timezone="${TZ:-Asia/Shanghai}" \
+  -jar /app/app.jar "$@"
